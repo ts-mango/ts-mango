80aa570a65ad768ad54cba8874a73f7efc6f3600
15227241216 <1469442737@qq.com>
2022-05-06 23:21:10 +0800

    docs: 更新文档

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { sidebar } from './utils/sidebar'
 
 const config = {
   title: 'TsMango',
+  base: '/mango/',
   description: 'TsMango 是一个用 typescript 编写的实用函数库。（TsMango is a utility function library written in typescript.）',
   themeConfig: {
     nav,
