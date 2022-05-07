83b1c7e77aa3fc35f531b2b6400e2a7eb42ee061
tyh2001 <1469442737@qq.com>
2022-05-07 13:56:05 +0800

    docs: 更新logo

--- docs/src/vitepress/components/VPNavBarTitle.vue
+++ docs/src/vitepress/components/VPNavBarTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="VPNavBarTitle" href="/mango">
+  <a class="VPNavBarTitle" href="/mango/index.html">
     <img class="logo" src="https://tianyuhao.cn/mango/images/mango.svg" />
     <span class="text">TsMango</span>
   </a>
