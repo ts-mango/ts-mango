81a08630db3700b4413059ed7d1b7b2ba03fa363
15227241216 <1469442737@qq.com>
2022-05-10 23:49:52 +0800

    docs: 更新导航栏 nav

--- docs/docs/.vitepress/utils/nav.ts
+++ docs/docs/.vitepress/utils/nav.ts
@@ -13,5 +13,10 @@ export const nav = [
     text: '更新日志',
     activeMatch: '^/docs/',
     link: '/docs/CHANGELOG'
+  },
+  {
+    text: 'Github',
+    activeMatch: 'https://github.com/Tyh2001/TsMango',
+    link: 'https://github.com/Tyh2001/TsMango'
   }
 ]
