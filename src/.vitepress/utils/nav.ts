8a69c6aefa1c32bc0d4db3c5d9ee4f9e441c30f5
tyh2001 <1469442737@qq.com>
2022-09-29 17:21:39 +0800

    chore: 更新构建配置 :hot_pepper::hot_pepper::hot_pepper:

--- src/.vitepress/utils/nav.ts
+++ src/.vitepress/utils/nav.ts
@@ -1,7 +1,7 @@
 export const nav = [
-  // {
-  //   text: '文档',
-  //   activeMatch: '/docs/',
-  //   link: '/docs/hello'
-  // }
+  {
+    text: '开始',
+    activeMatch: '/packages/',
+    link: '/packages/get-type/index'
+  }
 ]
