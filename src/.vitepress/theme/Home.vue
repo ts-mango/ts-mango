9288a3ac1cb44cde681928e3893446a0c425462c
tyh2001 <1469442737@qq.com>
2022-10-06 13:41:14 +0800

    docs: 更新文档首页

--- src/.vitepress/theme/Home.vue
+++ src/.vitepress/theme/Home.vue
@@ -1,50 +1,57 @@
 <template>
-  <div id="home">
-    <p align="center">
-      <img src="https://tianyuhao.cn/images/ts-mango/ts-mango.svg" />
-    </p>
+  <f-main>
+    <f-text center>
+      <f-image
+        width="160px"
+        src="https://tianyuhao.cn/images/ts-mango/ts-mango.svg"
+      />
+    </f-text>
+
     <p class="title">TsMango</p>
 
-    <p align="center">
-      <a href="/docs/install">
-        <button class="start">Start using</button>
-      </a>
-      <a href="https://github.com/ts-mango/ts-mango" target="_back">
-        <button>Github</button>
-      </a>
-    </p>
+    <f-text center>
+      <f-button round href="/docs/install" color="#f47c34" text-color="#fff">
+        Start using
+      </f-button>
+      <f-button round href="https://github.com/ts-mango/ts-mango">
+        Github
+      </f-button>
+    </f-text>
 
-    <p class="box">
-      <strong>Introduce</strong>
-      <span>This is a multifunctional mango.</span>
-      <span>TsMango is a utility function library written in typescript.</span>
-    </p>
+    <f-divider />
 
-    <p class="box">
-      <strong>Why TsMango?</strong>
-      <span>🔥 Use TypeScript</span>
-      <span>🔔 Strong type</span>
-      <span>✅ Import in as needed</span>
-      <span>✔️ Strict verification</span>
-      <span>💪 Robust code</span>
-      <span>🚩 Multi function</span>
-      <span>🚀 Improve development efficiency</span>
-    </p>
-  </div>
+    <f-text bold size="24px">Introduce</f-text>
+    <f-text>This is a multifunctional mango.</f-text>
+    <f-text>
+      TsMango is a utility function library written in typescript.
+    </f-text>
+
+    <f-divider />
+
+    <f-text bold size="24px">Why TsMango?</f-text>
+    <f-text>🔥 Use TypeScript</f-text>
+    <f-text>🔔 Strong type</f-text>
+    <f-text>✅ Import in as needed</f-text>
+    <f-text>✔️ Strict verification</f-text>
+    <f-text>💪 Robust code</f-text>
+    <f-text>🚩 Multi function</f-text>
+    <f-text>🚀 Improve development efficiency</f-text>
+  </f-main>
 </template>
 
 <style scoped>
-  #home {
+  .f-main {
     max-width: 620px;
     margin: 40px auto;
     box-sizing: border-box;
     padding: 40px;
     padding-bottom: 0;
   }
-  img {
-    width: 160px;
-    user-select: none;
+
+  .f-button-default {
+    margin-left: 15px;
   }
+
   .title {
     font-size: 25px;
     font-weight: 600;
@@ -53,44 +60,9 @@
     margin-bottom: 20px;
     user-select: none;
   }
-  button {
-    width: 120px;
-    height: 40px;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    background: #eee;
-    color: #333;
-    border-radius: 10px;
-    margin-bottom: 30px;
-    user-select: none;
-  }
-  .start {
-    margin-right: 15px;
-    background: rgb(244, 124, 52);
-    color: #fff;
-    font-weight: 600;
-  }
-  button:hover {
-    opacity: 0.8;
-  }
 
-  button:active {
-    opacity: 1;
-  }
-  .box {
-    padding: 40px 0;
-    border-top: 1px solid rgb(224, 224, 224);
-    box-sizing: border-box;
-  }
-  .box span {
+  .f-text {
     display: block;
     line-height: 40px;
-  }
-  .box strong {
-    font-size: 24px;
-    font-weight: 600;
-    margin-bottom: 20px;
-    display: block;
   }
 </style>
